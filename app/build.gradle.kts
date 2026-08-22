0d1e035e487ed8601859f1ff862544f2e7cee138
k25vsgvkmw-code <k25vsgvkmw@privaterelay.appleid.com>
2026-08-22 19:46:42 +0300

    Release AGNES TV v1.5.0 reliable launcher

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "mom.agnes.tv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.0"
+        versionCode = 15
+        versionName = "1.5.0"
     }
 
     compileOptions {
