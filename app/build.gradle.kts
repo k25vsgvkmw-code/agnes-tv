92bfd211ad74b07248fda1e220c5a5bb20aa8d39
k25vsgvkmw-code <k25vsgvkmw@privaterelay.appleid.com>
2026-08-22 18:55:20 +0300

    Release AGNES OS v1.4.0 home launcher

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "mom.agnes.tv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3.0"
+        versionCode = 14
+        versionName = "1.4.0"
     }
 
     compileOptions {
