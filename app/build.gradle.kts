db711107be1f9e5ad0ccc04221015feac9905528
k25vsgvkmw-code <k25vsgvkmw@privaterelay.appleid.com>
2026-08-27 16:53:27 +0300

    Bump AGNES TV to v1.7.2 auto-login build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "mom.agnes.tv"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.7.1"
+        versionCode = 21
+        versionName = "1.7.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
