dc91b9436d3bcce83e0625865dfe0229febb768b
k25vsgvkmw-code <k25vsgvkmw@privaterelay.appleid.com>
2026-08-29 13:29:57 +0300

    test: add v2 regression suite

---
+++ app/src/androidTest/java/mom/agnes/tv/V2RegressionSuite.kt
@@ -0,0 +1,10 @@
+package mom.agnes.tv
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    V2StartupRegressionTest::class
+)
+class V2RegressionSuite
