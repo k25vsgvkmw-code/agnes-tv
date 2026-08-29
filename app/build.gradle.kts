2fb3a04da73fe71cda1f023f17f6b05ea1de3100
k25vsgvkmw-code <k25vsgvkmw@privaterelay.appleid.com>
2026-08-29 13:48:09 +0300

    test: enable Xtream JVM regression tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,10 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.6.1")
     implementation("androidx.media3:media3-ui:1.6.1")
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
