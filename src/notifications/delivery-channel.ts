6d2b1982dfc6f51466621aa7eb05afea8b19a3d2
k25vsgvkmw-code <k25vsgvkmw@privaterelay.appleid.com>
2026-08-30 19:55:55 +0300

    feat: define Live v2 delivery channels

---
+++ src/notifications/delivery-channel.ts
@@ -0,0 +1,9 @@
+export type DeliveryChannel =
+  | 'SILENT_FEED'
+  | 'IN_APP'
+  | 'MOBILE_PUSH'
+  | 'TABLET_ALERT'
+  | 'TV_BANNER'
+  | 'VOICE_HOME'
+  | 'VOICE_PERSONAL_DEVICE'
+  | 'CRITICAL_ALARM';
